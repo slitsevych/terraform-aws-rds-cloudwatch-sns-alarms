a2451d74cead2bd986eaf50f3cc35bc4dd19e2df
Stepan Litsevych <s.litsevych@aimprosoft.com>
2023-07-22 13:40:48 +0300

    updated alarm descriptions and names

--- alarms.tf
+++ alarms.tf
@@ -11,7 +11,7 @@ locals {
 }
 
 resource "aws_cloudwatch_metric_alarm" "burst_balance_too_low" {
-  alarm_name          = "${var.name_prefix}-burst_balance_too_low"
+  alarm_name          = "${var.name_prefix}-Burst_Balance_Too_Low"
   comparison_operator = "LessThanThreshold"
   evaluation_periods  = var.evaluation_periods
   metric_name         = "BurstBalance"
@@ -19,7 +19,7 @@ resource "aws_cloudwatch_metric_alarm" "burst_balance_too_low" {
   period              = var.metric_alarm_period
   statistic           = "Average"
   threshold           = local.thresholds["BurstBalanceThreshold"]
-  alarm_description   = "Average database storage burst balance over last 10 minutes too low, expect a significant performance drop soon"
+  alarm_description   = "Average ${var.name_prefix} database storage burst balance over last 5 minutes is too low: expect a significant performance drop soon"
   alarm_actions       = var.use_sns_topic_for_alarms == true ? local.aws_sns_topic_arn : []
   ok_actions          = var.use_sns_topic_for_alarms == true ? local.aws_sns_topic_arn : []
 
@@ -29,7 +29,7 @@ resource "aws_cloudwatch_metric_alarm" "burst_balance_too_low" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "cpu_utilization_too_high" {
-  alarm_name          = "${var.name_prefix}-cpu_utilization_too_high"
+  alarm_name          = "${var.name_prefix}-CPU_Utilization_Too_High"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = var.evaluation_periods
   metric_name         = "CPUUtilization"
@@ -37,7 +37,7 @@ resource "aws_cloudwatch_metric_alarm" "cpu_utilization_too_high" {
   period              = var.metric_alarm_period
   statistic           = "Average"
   threshold           = local.thresholds["CPUUtilizationThreshold"]
-  alarm_description   = "Average database CPU utilization over last 10 minutes too high"
+  alarm_description   = "Average ${var.name_prefix} database CPU utilization over last 5 minutes is too high"
   alarm_actions       = var.use_sns_topic_for_alarms == true ? local.aws_sns_topic_arn : []
   ok_actions          = var.use_sns_topic_for_alarms == true ? local.aws_sns_topic_arn : []
 
@@ -47,7 +47,7 @@ resource "aws_cloudwatch_metric_alarm" "cpu_utilization_too_high" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "cpu_credit_balance_too_low" {
-  alarm_name          = "${var.name_prefix}-cpu_credit_balance_too_low"
+  alarm_name          = "${var.name_prefix}-CPU_Credit_Balance_Too_Low"
   comparison_operator = "LessThanThreshold"
   evaluation_periods  = var.evaluation_periods
   metric_name         = "CPUCreditBalance"
@@ -55,7 +55,7 @@ resource "aws_cloudwatch_metric_alarm" "cpu_credit_balance_too_low" {
   period              = var.metric_alarm_period
   statistic           = "Average"
   threshold           = local.thresholds["CPUCreditBalanceThreshold"]
-  alarm_description   = "Average database CPU credit balance over last 10 minutes too low, expect a significant performance drop soon"
+  alarm_description   = "Average ${var.name_prefix} database CPU credit balance over last 5 minutes is too low: expect a significant performance drop soon"
   alarm_actions       = var.use_sns_topic_for_alarms == true ? local.aws_sns_topic_arn : []
   ok_actions          = var.use_sns_topic_for_alarms == true ? local.aws_sns_topic_arn : []
 
@@ -65,7 +65,7 @@ resource "aws_cloudwatch_metric_alarm" "cpu_credit_balance_too_low" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "disk_queue_depth_too_high" {
-  alarm_name          = "${var.name_prefix}-disk_queue_depth_too_high"
+  alarm_name          = "${var.name_prefix}-Disk_Queue_Depth_Too_High"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = var.evaluation_periods
   metric_name         = "DiskQueueDepth"
@@ -73,7 +73,7 @@ resource "aws_cloudwatch_metric_alarm" "disk_queue_depth_too_high" {
   period              = var.metric_alarm_period
   statistic           = "Average"
   threshold           = local.thresholds["DiskQueueDepthThreshold"]
-  alarm_description   = "Average database disk queue depth over last 10 minutes too high, performance may suffer"
+  alarm_description   = "Average ${var.name_prefix} database disk queue depth over last 5 minutes is too high: performance may suffer"
   alarm_actions       = var.use_sns_topic_for_alarms == true ? local.aws_sns_topic_arn : []
   ok_actions          = var.use_sns_topic_for_alarms == true ? local.aws_sns_topic_arn : []
 
@@ -83,7 +83,7 @@ resource "aws_cloudwatch_metric_alarm" "disk_queue_depth_too_high" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "freeable_memory_too_low" {
-  alarm_name          = "${var.name_prefix}-freeable_memory_too_low"
+  alarm_name          = "${var.name_prefix}-Freeable_Memory_Too_Low"
   comparison_operator = "LessThanThreshold"
   evaluation_periods  = var.evaluation_periods
   metric_name         = "FreeableMemory"
@@ -91,7 +91,7 @@ resource "aws_cloudwatch_metric_alarm" "freeable_memory_too_low" {
   period              = var.metric_alarm_period
   statistic           = "Average"
   threshold           = local.thresholds["FreeableMemoryThreshold"]
-  alarm_description   = "Average database freeable memory over last 10 minutes too low, performance may suffer"
+  alarm_description   = "Average ${var.name_prefix} database freeable memory over last 5 minutes is too low: performance may suffer"
   alarm_actions       = var.use_sns_topic_for_alarms == true ? local.aws_sns_topic_arn : []
   ok_actions          = var.use_sns_topic_for_alarms == true ? local.aws_sns_topic_arn : []
 
@@ -101,7 +101,7 @@ resource "aws_cloudwatch_metric_alarm" "freeable_memory_too_low" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "free_storage_space_too_low" {
-  alarm_name          = "${var.name_prefix}-free_storage_space_threshold"
+  alarm_name          = "${var.name_prefix}-Free_Storage_Space_Low"
   comparison_operator = "LessThanThreshold"
   evaluation_periods  = var.evaluation_periods
   metric_name         = "FreeStorageSpace"
@@ -109,7 +109,14 @@ resource "aws_cloudwatch_metric_alarm" "free_storage_space_too_low" {
   period              = var.metric_alarm_period
   statistic           = "Average"
   threshold           = local.thresholds["FreeStorageSpaceThreshold"]
-  alarm_description   = "Average database free storage space over last 10 minutes too low"
+  alarm_description = format(
+    var.alarm_description,
+    "CPU",
+    "High",
+    var.cpu_utilization_high_period / 60,
+    var.cpu_utilization_high_evaluation_periods
+  )
+  alarm_description   = "Average ${var.name_prefix} database free storage space over last 5 minutes is too low"
   alarm_actions       = var.use_sns_topic_for_alarms == true ? local.aws_sns_topic_arn : []
   ok_actions          = var.use_sns_topic_for_alarms == true ? local.aws_sns_topic_arn : []
 
@@ -119,7 +126,7 @@ resource "aws_cloudwatch_metric_alarm" "free_storage_space_too_low" {
 }
 
 resource "aws_cloudwatch_metric_alarm" "swap_usage_too_high" {
-  alarm_name          = "${var.name_prefix}-swap_usage_too_high"
+  alarm_name          = "${var.name_prefix}-Swap_Usage_Too_High"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = var.evaluation_periods
   metric_name         = "SwapUsage"
@@ -127,7 +134,7 @@ resource "aws_cloudwatch_metric_alarm" "swap_usage_too_high" {
   period              = var.metric_alarm_period
   statistic           = "Average"
   threshold           = local.thresholds["SwapUsageThreshold"]
-  alarm_description   = "Average database swap usage over last 10 minutes too high, performance may suffer"
+  alarm_description   = "Average ${var.name_prefix} database swap usage over last 5 minutes is too high: performance may suffer"
   alarm_actions       = var.use_sns_topic_for_alarms == true ? local.aws_sns_topic_arn : []
   ok_actions          = var.use_sns_topic_for_alarms == true ? local.aws_sns_topic_arn : []
 
